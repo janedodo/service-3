8d0388c14e888cea9f08ccc8b19d0c5d0440a73c
janedodo <35283478+janedodo@users.noreply.github.com>
2018-01-24 11:26:35 +0800

    Update HelloworldDelegate.java

--- src/main/java/com/service/helloworld/controller/HelloworldDelegate.java
+++ src/main/java/com/service/helloworld/controller/HelloworldDelegate.java
@@ -9,6 +9,6 @@ public class HelloworldDelegate implements Helloworld {
     public String helloworld(String name){
 
         // Do Some Magic Here!
-        return "hi, " + name;
+        return "Hello, " + name;
     }
 }
